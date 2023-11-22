9df49cfde87feccaba6a8bb0b945d3dfe076327d
Aleksei da Silva <alekseidasilva@hotmail.com>
2023-11-22 22:13:46 +0100

    Trabalhando na Instrodução de Testes

--- src/ALAYSchoolManagment.Domain/Entidades/AnoAcademico.cs
+++ src/ALAYSchoolManagment.Domain/Entidades/AnoAcademico.cs
@@ -1,8 +1,33 @@
+using ALAYSchoolManager.Domain.Exceptions;
+
 namespace ALAYSchoolManager.Domain.Entidades;
 
 public class AnoAcademico
 {
     public int AnoAcademicoId { get; set; }
-    public string? AnoAcademicoDesignacao { get; set; }
+    public string? AnoAcademicoDesignacao { get; private set; }
+    public bool AnoAcademicoEstado { get; private set; }
+    public DateTime AnoAcademicoDataCriacao { get; private set; }
+
+    public void Actualizar(string anoAcademico)
+    {
+
+    }
+
+    private void Validar()
+    {
+        if (string.IsNullOrEmpty(AnoAcademicoDesignacao))
+            throw new EntityValidationException($"{nameof(AnoAcademicoDesignacao)} não pode ser vazio ou nulo");
+
+    }
+
+    public AnoAcademico(string anoAcademicoDesignacao, bool anoAcademicoEstado = true)
+    {
+        AnoAcademicoDesignacao = anoAcademicoDesignacao;
+        AnoAcademicoEstado = anoAcademicoEstado;
+        AnoAcademicoDataCriacao = DateTime.Now;
+        Validar();
+
+    }
 
 }
---
+++ src/ALAYSchoolManagment.Domain/Exceptions/EntityValidationException.cs
@@ -0,0 +1,9 @@
+﻿namespace ALAYSchoolManager.Domain.Exceptions;
+
+public class EntityValidationException : Exception
+{
+    public EntityValidationException(string? message) : base(message)
+    {
+
+    }
+}
