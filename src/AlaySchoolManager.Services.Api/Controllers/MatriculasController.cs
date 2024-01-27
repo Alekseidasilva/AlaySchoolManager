897e4f8e5ee0e3292e2c13655817e9b5c71f34c6
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-01-27 15:58:10 +0100

    FEAT: Adiconado Projecto API

---
+++ src/AlaySchoolManager.Services.Api/Controllers/MatriculasController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AlaySchoolManager.Services.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatriculasController : ControllerBase
+    {
+        // GET: api/<MatriculasController>
+        [HttpGet]
+        public IEnumerable<string> Matriculados()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MatriculasController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MatriculasController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MatriculasController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MatriculasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
